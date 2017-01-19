73f57351d96f2e06fee9a37156250edd7bdfcc46
jaytulk <jay.tulk@selu.edu>
2017-01-19 00:08:53 -0600

    Added simple factory.

---
+++ FactoryDesignPatterns/GroundBeef.cs
@@ -0,0 +1,33 @@
+﻿using static FactoryDesignPatterns.Helper;
+
+namespace FactoryDesignPatterns
+{
+    public enum Grade
+    {
+        Lean, Chuck, Medium
+    }
+
+    public abstract class GroundBeef
+    {
+        public abstract string Name { get; }
+        public abstract double Price { get; set; }
+    }
+
+    public class Lean : GroundBeef
+    {
+        public override string Name { get; } = "Lean Ground Beef";
+        public override double Price { get; set; } = 4.95;
+    }
+
+    public class Medium : GroundBeef
+    {
+        public override string Name { get; } = "Medium Ground Beef";
+        public override double Price { get; set; } = 3.49;
+    }
+
+    public class Chuck : GroundBeef
+    {
+        public override string Name { get; } = "Chuck Ground Beef";
+        public override double Price { get; set; } = 2.79;
+    }
+}
---
+++ FactoryDesignPatterns/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static FactoryDesignPatterns.Helper;
+
+namespace FactoryDesignPatterns
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Set(new[] { Options.G, Options.Simple }, true);
+
+            var simpleFactory = new SimpleMeatFactory();
+            var chuckBeef = simpleFactory.GetMeat(Grade.Chuck);
+            var mediumBeef = simpleFactory.GetMeat(Grade.Medium);
+            var leanBeef = simpleFactory.GetMeat(Grade.Lean);
+
+            Write($"The simple factory made: {chuckBeef.Name} for ${chuckBeef.Price}/lb\n");
+
+            Set(new[] { Options.C });
+
+            Write($"The simple factory made: {mediumBeef.Name} for ${mediumBeef.Price}/lb\n");
+
+            Set(new[] { Options.B });
+
+            Write($"The simple factory made: {leanBeef.Name} for ${leanBeef.Price}/lb\n");
+
+            Console.ReadLine();
+        }
+    }
+}
