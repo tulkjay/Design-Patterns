a269db878c601305561b7df495d8f5b4d9cbec53
jtulk <jay.tulk@selu.edu>
2017-03-03 08:36:44 -0600

    Added simple factory to master solution.

---
+++ Simple_Factory/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Simple_Factory.Core;
+using static Simple_Factory.Setup.Helper;
+
+namespace Simple_Factory
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Set(new[] { Options.G, Options.Simple }, true);
+
+            var simpleFactory = new SimpleMeatFactory();
+
+            var chuckBeef = simpleFactory.GetMeat(Grade.Chuck);
+            var mediumBeef = simpleFactory.GetMeat(Grade.Medium);
+            var leanBeef = simpleFactory.GetMeat(Grade.Lean);
+
+            Write($"The simple factory made: {chuckBeef.Name} for ${chuckBeef.Price}/lb\n");
+
+            Set(new[] { Options.C });
+
+            Write($"The simple factory made: {mediumBeef.Name} for ${mediumBeef.Price}/lb\n");
+
+            Set(new[] { Options.B });
+
+            Write($"The simple factory made: {leanBeef.Name} for ${leanBeef.Price}/lb\n");
+
+            Console.ReadLine();
+        }
+    }
+}
