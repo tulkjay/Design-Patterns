cab23b0f09655f92645cd1895573bb77cf0f469b
tulkjay <jay.tulk@selu.edu>
2018-01-31 00:32:16 -0600

    feat(wip): Adding bridge design pattern program.

---
+++ Bridge/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using Bridge.Setup;
+
+namespace Bridge
+{
+  public static class Program
+  {
+    static void Main()
+    {
+      Helper.SetTitle();
+
+      /*
+        var chargingService = new CreditCustomerService();
+        chargingService.Service = new RoomOrderingService();
+        chargingService.Charge();
+
+        chargingService.Service = new CheckoutService();
+        chargingService.Charge();
+
+        var specializedService = new RoomServiceService();
+        specializedService.Charge();
+
+        specializedService = new BreakfastInBedService();
+        specializedService.Charge();
+
+        specializedService.Service = new CheckoutService();
+        specializedService.Charge();
+    */
+
+      //The bridge design pattern decouples an abstraction from its implementation so that the two can vary independently.
+
+      if (Debugger.IsAttached)
+      {
+        Console.ReadLine();
+      }
+    }
+  }
+
+  public abstract class ServiceProvider
+  {
+    public abstract void PerformService();
+  }
+  public class PayableServices
+  {
+    protected ServiceProvider _provider;
+    public ServiceProvider Provider
+    {
+      set { _provider = value; }
+    }
+
+    public virtual void Complete()
+    {
+      _provider.PerformService();
+    }
+  }
+
+  public class SpecializedServices : PayableServices
+  {
+    public override void Complete()
+    {
+      _provider.PerformService();
+    }
+  }
+}
---
+++ Bridge/Setup/Helper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Bridge.Setup
+{
+    public static class Helper
+    {
+        public const string Welcome = "This example follows the bridge design pattern";
+        public static ConsoleColor CurrentConsoleColor;
+        public static void SetTitle()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("\n\t================================\n\t=  ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(" Frank's Housing Service    ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("=\n\t================================\n\n");
+            Console.WriteLine(Welcome);
+            Set(ConsoleColor.Cyan);
+            Console.WriteLine();
+        }
+
+        public static void Set(ConsoleColor option)
+        {
+            CurrentConsoleColor = option;
+            Console.ForegroundColor = CurrentConsoleColor;
+        }
+
+        public static void Hr(bool newline = true)
+        {
+            Write(string.Empty.PadLeft(70, '=') + $"{(newline ? "\n" : "")}");
+        }
+
+        public static void Write(string text = "", ConsoleColor? startColor = null)
+        {
+            Set(startColor ?? CurrentConsoleColor);
+            Console.WriteLine($"{text}");
+        }
+    }
+}
